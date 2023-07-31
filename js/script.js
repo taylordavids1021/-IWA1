4855d81b11511c9a2ea779f3a097522f40aaf025
Taylor <taylordavids997@gmail.com>
2023-07-31 17:14:56 +0200

    add subtract and add value

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@ const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
 
+// parseInt is a counts for every click number.value eirther more or less
+// SubtractHandle holds newValue data
 const subtractHandler = () => {
-    console.log(typeof number.value)
-    number.value = number.value - 1;
+    const newValue = parseInt(number.value) - 1
+    number.value = newValue;
 }
-
+// addHandle holds newValue data
 const addHandler = () => {
-    number.value = number.value + 1;
+    const newValue = parseInt(number.value) + 1
+    number.value = newValue
 }
 
 subtract.addEventListener('click', subtractHandler)
