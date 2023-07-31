00c2a9f3f43257123eb2fdc8b26f1515fb559f47
Taylor <taylordavids997@gmail.com>
2023-07-31 17:24:13 +0200

    add if statement if value is true change opasity

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
+const MAX_NUMBER = 15;
+const MIN_NUMBER = -5;
+const STEP_AMOUNT = 5;
 
 // parseInt is a counts for every click number.value eirther more or less
 // SubtractHandle holds newValue data
 const subtractHandler = () => {
     const newValue = parseInt(number.value) - 1
     number.value = newValue;
+
+    if (add.disabled === true){
+        add.disabled = false 
+    }
+
+    if (newValue <= MIN_NUMBER){
+        subtract.disabled = true 
+    }
 }
 // addHandle holds newValue data
 const addHandler = () => {
     const newValue = parseInt(number.value) + 1
     number.value = newValue
+
+    if (subtract.disabled === true){
+        subtract.disabled = false 
+    }
+
+    if (newValue => MIN_NUMBER){
+        add.disabled = true 
+    }
 }
 
 subtract.addEventListener('click', subtractHandler)
